003d86daf0d6d06518f23a7de5df623ce49ee502
Zlatin <zlatin.stanimirovv@gmail.com>
2022-09-04 13:27:06 +0300

    feat: initial commit

---
+++ primer/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    jest: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': ['warn'],
+  },
+  ignorePatterns: ['*example*', '*dist*', '*test*'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
