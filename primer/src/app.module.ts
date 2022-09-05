10cdc7d43df638e17d6432c5cb6e699450461775
Zlatin <zlatin.stanimirovv@gmail.com>
2022-09-05 19:34:40 +0300

    feat: add config service to app.service

--- primer/src/app.module.ts
+++ primer/src/app.module.ts
@@ -1,10 +1,18 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {AppConfigService} from './config/appconfig.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppConfigService],
 })
 export class AppModule {}
--- primer/src/app.service.ts
+++ primer/src/app.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from '@nestjs/common';
+import {AppConfigService} from './config/appconfig.service';
 
 @Injectable()
 export class AppService {
+  constructor(private appConfigService: AppConfigService) {}
+
   getHello(): string {
+    console.log(this.appConfigService.getConfig<number>('port'));
     return 'Hello World!';
   }
 }
