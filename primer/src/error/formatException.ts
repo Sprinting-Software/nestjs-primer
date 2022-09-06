130b50c770b34247f27740571efc5a9c6e9e460e
Zlatin <zlatin.stanimirovv@gmail.com>
2022-09-06 12:53:52 +0300

    feat: add logs to globalError filter

---
+++ primer/src/error/formatException.ts
@@ -0,0 +1,10 @@
+import {HttpArgumentsHost} from '@nestjs/common/interfaces';
+import {BaseError} from './error.base';
+
+export default function formatException(error: BaseError<any>, ctx: HttpArgumentsHost): string {
+  const request = ctx.getRequest();
+
+  return `name: ${error.name}, message: ${error.message}, errorData: ${JSON.stringify(
+    error.data,
+  )} requestBody: ${JSON.stringify(request.body)}, requestHeaders: ${JSON.stringify(request.headers)}`;
+}
--- primer/src/error/globalError.filter.ts
+++ primer/src/error/globalError.filter.ts
@@ -1,6 +1,8 @@
 import {ArgumentsHost, Catch, ExceptionFilter, Injectable} from '@nestjs/common';
 import {AppConfigService} from '..//config/appconfig.service';
+import {logger} from '../logger/mylogger';
 import {BaseError} from './error.base';
+import formatException from './formatException';
 
 @Injectable()
 @Catch()
@@ -8,7 +10,26 @@ export class GlobalErrorFilter implements ExceptionFilter {
   constructor(private readonly appConfig: AppConfigService) {}
 
   catch(exception: Error | BaseError<any>, host: ArgumentsHost) {
-    //TODO
-    console.log('CAUGHT ERROR!!!!!', exception);
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    if (exception instanceof BaseError) {
+      logger.error(__filename, `Caught Error: ${formatException(exception, ctx)} `);
+      // TODO: add event
+
+      response.status(exception.status).send({
+        message: exception.message,
+      });
+    } else if (exception instanceof Error) {
+      // Code shouldn't normally come here. All errors should be captured on the edge of the system and transformed
+      // into classes derived from BaseError. When this code is entered it means the error needs tobe captured and transformed.
+      logger.error(__filename, `Caught unhandled exception ${exception.toString()}`);
+      response.status(500).send({message: exception.toString()});
+    } else {
+      // Code should never come here. It means there is an unhandled exception which needs to be captured and parsed
+      // In other words every time this path is executed it signifies a bug in the codebase
+      logger.error(__filename, `Caught unhandled exception ${JSON.stringify(exception)}`);
+      response.status(500).send({message: 'Internal Error'});
+    }
   }
 }
